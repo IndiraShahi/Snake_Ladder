f76e9988d5def6c26fce916ae5e3ee6b35129ea1
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-03 01:20:59 +0530

    [Indira]:ADDED:Snake or Ladder or No play

--- Snake_Ladder/Snake_Ladder/Game.cs
+++ Snake_Ladder/Snake_Ladder/Game.cs
@@ -9,11 +9,30 @@ namespace Snake_Ladder
         public void SnakeLadder()
         {
             int start = 0;
+            const int For_Ladder = 1;
+            const int For_Snake = 2;
+
             Console.WriteLine("Start position of player is : " + start);
             Random dice = new Random();
             int diceRoll = dice.Next(1, 7);
             Console.WriteLine("Player dice number is  : " + diceRoll);
+            int SnakeOrLadder = dice.Next(0, 3);
+            switch (SnakeOrLadder)
+            {
+                case For_Ladder:
+                    start += diceRoll;
+                    Console.WriteLine("Player moves forward");
+                    break;
 
+                case For_Snake:
+                    start -= diceRoll;
+                    Console.WriteLine("Player moves back");
+                    break;
+
+                default:
+                    Console.WriteLine("No Play");
+                    break;
+            }
         }
     }
 }

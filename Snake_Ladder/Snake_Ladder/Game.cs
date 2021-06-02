c3341667e04e7d028eb23c8a8f62366e6f9642e6
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-03 01:14:24 +0530

    [Indira]:ADDED:Dice Roll Feature

--- Snake_Ladder/Snake_Ladder/Game.cs
+++ Snake_Ladder/Snake_Ladder/Game.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Snake_Ladder
 {
     class Game
     {
-        public int start = 0;
         public void SnakeLadder()
         {
+            int start = 0;
             Console.WriteLine("Start position of player is : " + start);
+            Random dice = new Random();
+            int diceRoll = dice.Next(1, 7);
+            Console.WriteLine("Player dice number is  : " + diceRoll);
+
         }
     }
 }
